e43b15b24020479c13e08c6dcd852668a8dc115d
Carlos Echeverria <carlosechev07@gmail.com>
2015-12-07 16:44:32 -0800

    Adding  module for CLI handling. Adding  module for better console output.

--- bin/reall-cli.js
+++ bin/reall-cli.js
@@ -3,31 +3,68 @@
 /**
  *
  */
-
 var reall = require('reall'),
-    path = require("path")
+    program = require('commander'),
+    pretty = require('pretty-message'),
+    path = require("path"),
+    pkg = require('../package.json')
     ;
-console.log("Hi, this is the Reall command line in action");
-console.log("current dir ", process.cwd());
 
-var reallPath = '/usr/lib/node_modules/reall';
+//console.log("Hi, this is the Reall command line in action");
+//console.log("current dir ", process.cwd());
 
-//cliHelper.runCmd((os.platform() == 'win32' ? '' : 'sudo ') + 'npm install -g grunt-cli', "Installing grunt:", false, function (error, stdout, stderr) {
-//runCmd("ls -ltr", "", false, function (error, stdout, stderr) {
-//reall.runCmd("", "hs /home/mapr/trainning/code/mapperByWeekday.py /home/mapr/trainning/code/reducerByWeekday.py input/purcha*txt out/mapperByWeekday/00007", false, function (error, stdout, stderr) {
-//reall.runCmd("./bin/stream", "/home/mapr/trainning/code/mapperByWeekday.py /home/mapr/trainning/code/reducerByWeekday.py input/purcha*txt out/mapperByWeekday/00007", false, function (error, stdout, stderr) {
-//reall.runCmd(path.join(reallPath, "bin/stream.sh"), "/home/mapr/trainning/code/mapperByWeekday.py /home/mapr/trainning/code/reducerByWeekday.py input/purcha*txt out/mapperByWeekday/00007", false, function (error, stdout, stderr) {
 
-reall.hadoop.mapReduce("/home/mapr/trainning/code/mapperByWeekday.py", "/home/mapr/trainning/code/reducerByWeekday.py", "input/mapperByWeekday/purchases_100.txt", "out/mapperByWeekday", true, function (error, stdout, stderr) {
-    if (error) {
-	console.log("Fail runnig mapReduce");
-	console.log("%s", error);
-	data = "Fail running mapReduce: " + error;
+
+// Get version number from package.json file
+program.version(pkg.version);
+
+/**
+ * TODO:
+ *  1.  Add code to control 'no parameters send by user'
+ */
+
+program.command('*')
+    .action(function (env) {
+	pretty.alert();
+	pretty.alert("Enter a Valid command");
+	pretty.alert("   To see help use: reall -h");
+    });
+
+/**
+ * Restart command
+ */
+program.command('hd')
+    .description('Hadoop tools for real time data display')
+    .option("-r, --reduce", "Runs the standar command for Hadoop MapReduce")
+    .action(function (options) {
+	pepe();
     }
-    else {
-	data = "Ok process: " + stdout;
-	console.log("%s", data);
-    }
-	
-    //callback(data /*<<== "data buffer to be saved on session"*/);
-});
+);
+
+
+function pepe() {
+    var reallPath = '/usr/lib/node_modules/reall';
+    
+    reall.hadoop.mapReduce("Running the Reall haddop tool", "/home/mapr/trainning/code/mapperByWeekday.py", "/home/mapr/trainning/code/reducerByWeekday.py", 
+			   "input/mapperByWeekday/purchases_100.txt", "out/mapperByWeekday", false, function (error, stdout, stderr) {
+
+	pretty.alert();
+
+	if (error) {
+	    pretty.failed("Fail runnig mapReduce");
+	    pretty.inform("%s", error);
+	    data = "Fail running mapReduce: " + error;
+	}
+	else {
+	    //data = "Ok process: " + stdout;
+	    pretty.done("MapReduce wass susessfully executed");
+	    pretty.inform("Please see output on Hadoop file system: %s", "out/mapperByWeekday");
+	}
+	    
+	//callback(data /*<<== "data buffer to be saved on session"*/);
+    });
+}
+
+// Invoke the command execution
+program.parse(process.argv);
+
--- bin/stream.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# HadoopStreamingJar=/opt/./mapr/hadoop/hadoop-2.7.0/share/hadoop/tools/lib/hadoop-streaming-2.7.0-mapr-1506.jar
-# HadoopStreamingJar=/opt/./mapr/hadoop/hadoop-2.7.0/share/hadoop/tools/sources/hadoop-streaming-2.7.0-mapr-1506-test-sources.jar
-# HadoopStreamingJar=/opt/./mapr/hadoop/hadoop-2.7.0/share/hadoop/tools/sources/hadoop-streaming-2.7.0-mapr-1506-sources.jar
-HadoopStreamingJar=/opt/./mapr/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-dev-streaming.jar
-
-#hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.1.1.jar -mapper $1 -reducer $2 -file $1 -file $2 -input $3 -output $4
-hadoop jar $HadoopStreamingJar -mapper $1 -reducer $2 -file $1 -file $2 -input $3 -output $4
